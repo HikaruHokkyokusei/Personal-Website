3c1f9f8e24f59615f311cbd0ad70f24cf4971865
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2023-03-20 02:48:42 +0530

    Updated WebSocketManager.go to better handle connecting and disconnecting client.

--- server/WebSocket/WebSocketManager.go
+++ server/WebSocket/WebSocketManager.go
@@ -6,11 +6,46 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
+	"github.com/google/uuid"
 )
 
-type WsMessage struct {
+type WebSocketClient struct {
+	uid  string
+	conn *websocket.Conn
+}
+type WsClientMessage struct {
+	uid   string
 	event string
 	data  any
+}
+
+var (
+	connectedClients = map[string]*websocket.Conn{}
+	clientMessages   = make(chan WsClientMessage, 1000)
+	registerClient   = make(chan WebSocketClient, 5)
+	unregisterClient = make(chan string, 5)
+)
+
+func runClientHandler() {
+	for {
+		select {
+		case webSocketClient := <-registerClient:
+			connectedClients[webSocketClient.uid] = webSocketClient.conn
+			fmt.Println("New Connection:", webSocketClient.uid)
+
+		case clientId := <-unregisterClient:
+			delete(connectedClients, clientId)
+			fmt.Println("Disconnected:", clientId)
+
+		case clientMessage := <-clientMessages:
+			handleWebSocketMessage(clientMessage)
+		}
+	}
+}
+
+func handleWebSocketMessage(wsClientMessage WsClientMessage) {
+	switch wsClientMessage.event {
+	}
 }
 
 func ConfigureWebsocket(server *fiber.App) {
@@ -21,54 +56,46 @@ func ConfigureWebsocket(server *fiber.App) {
 		return ctx.SendStatus(fiber.StatusUpgradeRequired)
 	})
 
+	go runClientHandler()
+
 	server.Get("/ws", websocket.New(func(conn *websocket.Conn) {
-		fmt.Println("New Connection")
-		var isCloseMessage = false
+		var uid = uuid.New().String()
+		registerClient <- WebSocketClient{uid, conn}
 
 		defer func(conn *websocket.Conn) {
+			unregisterClient <- uid
 			var err = conn.Close()
 			if err != nil {
-				fmt.Println("Error when trying to close the connection...")
+				fmt.Println("Error when trying to close the connection for client:", uid)
 			}
 		}(conn)
+
+		var isCloseMessage = false
 		conn.SetCloseHandler(func(code int, text string) error {
 			isCloseMessage = true
-			fmt.Println("Connection Closed. Code:", code, "Message:", text)
 			return nil
 		})
 
 		var (
-			mt        int
-			msg       []byte
-			err       error
-			wsMessage WsMessage
+			msg  []byte
+			err  error
+			temp map[string]any
 		)
 		for {
-			if mt, msg, err = conn.ReadMessage(); err != nil {
+			if _, msg, err = conn.ReadMessage(); err != nil {
 				if !isCloseMessage {
 					fmt.Println("Error when reading the message:", err)
 				}
 				break
 			}
 
-			var temp map[string]any
+			temp = make(map[string]any)
 			var err = json.Unmarshal(msg, &temp)
 			if err != nil {
 				fmt.Println("Error when parsing the websocket message")
 				continue
 			}
-			wsMessage = WsMessage{temp["event"].(string), temp["data"]}
-			handleWebSocketMessage(wsMessage)
-
-			if err = conn.WriteMessage(mt, msg); err != nil {
-				fmt.Println("Error when sending message:", err)
-				break
-			}
+			clientMessages <- WsClientMessage{uid, temp["event"].(string), temp["data"]}
 		}
 	}))
-}
-
-func handleWebSocketMessage(wsMessage WsMessage) {
-	switch wsMessage.event {
-	}
 }
