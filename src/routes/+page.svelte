c9fe8937a29ab09eaca29a8a86273850458046cd
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2023-03-22 04:21:04 +0530

    Updates to fix UI issues with firefox. BRUH...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
     import MediaQuery from "$lib/services/MediaQuery.svelte";
     import { personalData } from "../lib/configs/PersonalData";
+
+    let s1ContentHolderWidth = 0, s1ContentHolderHeight = 0, s1ContentWrapperWidth;
+
+    $: {
+        s1ContentWrapperWidth = Math.floor(s1ContentHolderWidth - (s1ContentHolderHeight * 8 / 9));
+    }
 </script>
 
 <svelte:head>
@@ -11,8 +17,15 @@
     <MediaQuery query="only screen and (min-aspect-ratio: 16 / 9)" let:matches>
         <svelte:fragment>
             <div class="MainPageSectionOneHolder" style="{matches ? 'height' : 'width' }: 100%;">
-                <video src="/videos/main-page-bg.mkv" muted loop autoPlay></video>
-                <div class="SectionOneContentHolder">
+                <video muted loop autoPlay>
+                    <source src="/videos/main-page-bg.webm" type="video/webm">
+                </video>
+                <div class="SectionOneContentHolder"
+                     bind:clientWidth={s1ContentHolderWidth} bind:clientHeight={s1ContentHolderHeight}>
+                    <div class="CenterColumnFlex SectionOneContentWrapper"
+                         style="--s1-content-wrapper-width: {s1ContentWrapperWidth}px;">
+                        <h1>Kon'nichiwa Gojin</h1>
+                    </div>
                 </div>
             </div>
         </svelte:fragment>
@@ -61,5 +74,14 @@
         top: 0;
         left: 0;
         z-index: 3;
+    }
+
+    .SectionOneContentWrapper {
+        width: var(--s1-content-wrapper-width);
+        height: 100%;
+
+        position: absolute;
+        top: 0;
+        right: 0;
     }
 </style>
